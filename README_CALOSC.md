d425b9bd81cbd58a22e6568d809812d89bae5b4d
ZielinskiPP <piotr.zielinski@uni.wroc.pl>
2015-11-05 17:10:27 +0100

    pocięte obserwacje, wykresy z aktywnoscią, wykres z pogoniami

---
+++ README_CALOSC.md
@@ -0,0 +1,14 @@
+---
+title: "README_CALOSC"
+author: "PZ"
+date: "Thursday, November 05, 2015"
+output: html_document
+---
+
+calosc -  tabela surowa, bez rozbitych obserwacji na poszczególne nietoperze.
+
+calosc_zdarzenia - tabela surowa, bez rozbitych obserwacji ale ze policzonymi typami zachowań
+
+calosc_split - tabela z pociętymi obserwacjami na poszczególne nietoperze
+
+calosc_split_zdarzenia - jw. plus policzone literki
--- analiza_calosc.R
+++ analiza_calosc.R
@@ -10,10 +10,13 @@ source("functions/count_instances.R")
 source("functions/u_remove.R")
 source("functions/bs_remove.R")
 source("functions/get_sun.R")
+source("functions/wesolowski_plot.R")
 
 library(dplyr)
 library(lubridate)
 library(ggplot2)
+library(lattice)
+library(tidyr)
 
 
 # Wczytywanie dancyh o słońcu
@@ -37,6 +40,14 @@ rm(jablow)
 rm(krajanow)
 
 
+## DOdawanie zmiennej "kolonia"
+
+calosc$Season <- toupper(calosc$Season)
+
+calosc<- separate(calosc,Season, c("kolonia", "Season"), "_")
+calosc$Season[calosc$Season == "1"] <- "I" 
+calosc$Season[calosc$Season == "2"] <- "II" 
+calosc$Season[calosc$Season == "3"] <- "III" 
 ## Zmieniam daty na format R - patrz lubridate
 calosc$Date_time <- dmy_hms(paste(calosc$Data, calosc$Time))
 calosc$Date_time <- calosc$Date_time -  minutes(60)
@@ -47,64 +58,152 @@ calosc$Part_night <- factor(ifelse(calosc$godzina>=12, "wieczor", "rano"))
 calosc$Part_night <- relevel(calosc$Part_night, "wieczor")
 
 # Czyścimy tabelę ze zbędnych kolumn 
-calosc <- select(calosc, Season, Phase, Date_time,Channel, Night, Part_night,
+calosc <- select(calosc, kolonia, Season, Phase, Date_time,Channel, Night, Part_night,
                  Event_V, Toys, Quantity)
 
 # Dodajemy dane o słońcu 
 calosc$date <- as.Date(calosc$Date_time)
 calosc <- left_join(calosc, sun, by ="date" )
 
-calosc <- mutate(calosc,after_down = Date_time - sunset,
-                 till_dusk = sunrise - Date_time)
+calosc <- mutate(calosc,after_dusk = Date_time - sunset,
+                 till_dawn = sunrise - Date_time)
 
 
 
-plot(calosc$date,calosc$after_down)
+
+
+#histogram(as.numeric(calosc$after_dusk[calosc$Part_night=="wieczor"])/60)
+
+#histogram(as.numeric(calosc$till_dawn[calosc$Part_night=="rano"]))
 
 
 
-histogram(as.numeric(calosc$after_down[calosc$Part_night=="wieczor"])/60)
+#hist_act <- ggplot(calosc[calosc$Part_night == "wieczor",], aes(x = as.numeric(after_dusk)/60))
+#hist_act + geom_histogram(binwidth = 5) + facet_grid(. ~ Season)
 
-histogram(as.numeric(calosc$till_dusk[calosc$Part_night=="rano"]))
+#hist_act <- ggplot(calosc[calosc$Part_night == "rano",], aes(x = as.numeric(till_dawn)))
+#hist_act + geom_histogram(binwidth = 5) + facet_grid(. ~ Season) + 
+    #scale_x_continuous(limits = c(0, 150))
 
 
-
-hist_act <- ggplot(calosc[calosc$Part_night == "wieczor",], aes(x = as.numeric(after_down)/60))
-hist_act + geom_histogram(binwidth = 5) + facet_grid(. ~ Season)
-
-hist_act <- ggplot(calosc[calosc$Part_night == "rano",], aes(x = as.numeric(till_dusk)/60))
-hist_act + geom_histogram() + facet_grid(. ~ Season)
 ## Usuwamy dane zawierające oberwacje niepełne (u) oraz
 # obserwacje duże i male (b i s), bez podmianki wewnętrzych
 # elementów na "/", kontroluje to parametr slash
 
 calosc <- u_remove(calosc, slash = F)
 calosc <- bs_remove(calosc, slash = F)
+calosc <- filter(calosc, Event_V != "")
 
 ## Liczymy zdarzenia w obrębie każdego kodu
 
-characters <-c("a","l","t","r","p","c","d","f","n","e", "u", "g", "h", "i",
-               "k","o", "w", "v")
+characters <-c("a","v","c","l","t","o","d","f","w","n","e", "h", "g","r",
+               "i", "k","q","p")
 
 zdarzenia <- count_instances(calosc, characters)
+calosc_zdarzenia <- cbind(calosc, zdarzenia)
 
 
 
-
-
-
-
-
+# Zamieniamy dane o pogoni na zmienną logiczną
 
 p <- zdarzenia$p
 p[is.na(p)] <-0
-x <- table(as.logical(calosc$Quantity-1), as.logical(p))
 
-chisq.test(x)
+calosc_zdarzenia$czy_pogon <- as.logical(p)
+
+# ZLiczamy ilość elementów w sekwencji
+calosc_zdarzenia$ilosc_elementow <- apply(calosc_zdarzenia[,16:29],1,sum, na.rm = T)
+calosc_zdarzenia <- mutate(calosc_zdarzenia,
+                           srednia_ilosc_elementow  = ilosc_elementow/Quantity)
+
+
+
+
+
+
+# Sprawdzamy czy obserwacje z pogoniami są bardziej 
+# złożone od obserwacji 2 niezależnych osobników
+#sub <- filter(calosc_zdarzenia, Quantity>1)
+#sub_plot <- ggplot(calosc_zdarzenia, aes(y = srednia_ilosc_elementow, x = czy_pogon))
+#sub_plot + geom_boxplot() + facet_grid(.~Season)
+
+
+### TNIEMY TABELĘ - JEDEN NIETOPERZ NA JEDNĄ OBSERWACJĘ
+
+calosc_split <- observation_split(calosc)
+
+# LIczymy poszczególne zdarzenia
+
+characters <-c("a","v","c","l","t","o","d","f","w","n","e", "h", "g","r",
+               "i", "k","q","p")
+
+zdarzenia <- count_instances(calosc_split, characters)
+calosc_split_zdarzenia <- cbind(calosc_split, zdarzenia)
+
+calosc_split_zdarzenia$ilosc_elementow <- apply(calosc_split_zdarzenia[,16:29],1,sum, na.rm = T)
+
+calosc_split_zdarzenia$shannon <- apply(as.matrix(zdarzenia), 1, function(x){
+    suma <- sum(x, na.rm = T)
+    prawd <- x/suma
+    s <- -1 * sum(log(prawd^prawd), na.rm = T)
+    s
+})
+
+
+shan_plot <- ggplot(calosc_split_zdarzenia, aes(x = Part_night, y = shannon))
+
+shan_plot + geom_boxplot(aes(x = Channel)) + facet_grid(kolonia~Season)
+
+
+
+
+
+
+# Grupujemy po sezonach, nocach i porze dnia, żeby potem policzyć sumy dla konkretnych
+# zdarzeń
+calosc_split_zdarzenia <- group_by(calosc_split_zdarzenia, kolonia, Season, Night, Part_night)
+
+### Liczymy liczbę pościgów i liczbę zdarzeń
+
+summary_calosc <- summarise_each(calosc_split_zdarzenia,
+                                 funs(
+                                     sum(., na.rm = T)
+                                 ), vars = -c(1:15))
+
+
+# Liczymy średnią liczbę pościgów
+# Ilość obserwacji
+summary_calosc$n_observation <- summarise(calosc_split_zdarzenia, n_observ = n())$n_observ 
+
+# Dodajemy średnie ilości pościgów 
+summary_calosc <- mutate(summary_calosc, mean_p = p/n_observation)    
+
+# Średnie złożoności, czyli średnia ilość elementów w pojedynczej obserwacji
+summary_calosc$srednia_sekwencja <- summarise(calosc_split_zdarzenia,
+                                              srednia_sekwencja = mean(ilosc_elementow))$srednia_sekwencja
+summary_calosc$sredni_shannon <- summarise(calosc_split_zdarzenia,
+                                              sredni_shannon = mean(shannon))$sredni_shannon
+
+
+## OGÓLNA AKTYWNOŚĆ
+akt_gg <- ggplot(summary_calosc, aes(y = n_observation, x = Part_night))
+akt_gg + geom_boxplot() + facet_grid(kolonia~Season) + theme_wesolowski()
+
+
+## Średnia ilość pogoni
+pogon_gg <- ggplot(summary_calosc, aes(y = mean_p, x = Part_night))
+pogon_gg + geom_boxplot() + facet_grid(kolonia~Season) + theme_wesolowski()
+
+## Przeloty_grupowe
+sub <- filter(calosc_zdarzenia, Quantity >1)
+grupy <- ggplot(sub,
+                aes(x = as.factor(Quantity),
+                    fill=czy_pogon ))
+grupy + geom_bar() + facet_grid(.~Season)
 
 
 ### BRUDY
-x <- 
+ 
 calosc %>% mutate(p = p) %>% filter(p == 1, Quantity == 1) -> dziwy
 
 which(calosc$Quantity == 1 & p ==1)
